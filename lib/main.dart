d871095af89bad0652647c79ae05eb3a2c48119d
maha othman <yeolbyun2002@gmail.com>
2023-11-13 14:48:50 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:math';
+//mm
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: const Text("تتطابق صورة"),
+          backgroundColor: Colors.indigo[800],
+        ),
+        body: const ImagePage(),
+      ),
+    );
+  }
+}
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({super.key});
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  int leftImageNumber = 1;
+  int rightImageNumber = 1;
+
+  changeImage() {
+    rightImageNumber = Random().nextInt(8) + 1;
+    leftImageNumber = Random().nextInt(8) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          leftImageNumber == rightImageNumber ? "You are win" : "Try Againe",
+          style: const TextStyle(fontSize: 40.0, color: Colors.white),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset("images/image-$leftImageNumber.png"),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset("images/image-$rightImageNumber.png"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
